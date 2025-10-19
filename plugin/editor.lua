99fb38fd3f6c75e43f983d94316fa6a7f705dad4
Stefan Genov <sgenov94@gmail.com>
2025-10-19 22:26:50 +0300

    feat: Experimental updates for writes

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -75,3 +75,8 @@ vim.opt.maxmempattern = 50000
 -- Cmd line
 -- This disables the cmdline
 -- vim.opt.cmdheight = 0
+
+
+-- Optimize file reading
+vim.opt.fsync = false -- disable fsync for faster writes
+vim.opt.swapsync = "" -- disable swap sync
