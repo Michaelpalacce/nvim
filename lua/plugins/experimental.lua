0a9c7f502785592561520989ae2af8d90bc882cf
Stefan Genov <sgenov94@gmail.com>
2025-10-25 17:53:14 +0300

    feat: Add dropbar
    Using it so I can see which file I am on in the top left

--- lua/plugins/experimental.lua
+++ lua/plugins/experimental.lua
@@ -1,16 +1,27 @@
 return {
 	{
 		'Bekaboo/dropbar.nvim',
-		-- optional, but required for fuzzy finder support
-		dependencies = {
-			'nvim-telescope/telescope-fzf-native.nvim',
-			build = 'make'
+		opts = {
+			bar = {
+				-- Display only the file path
+				sources = function(buf, _)
+					local sources = require('dropbar.sources')
+					local utils = require('dropbar.utils')
+					if vim.bo[buf].ft == 'markdown' then
+						return {
+							sources.path,
+						}
+					end
+					if vim.bo[buf].buftype == 'terminal' then
+						return {
+							sources.terminal,
+						}
+					end
+					return {
+						sources.path,
+					}
+				end,
+			},
 		},
-		config = function()
-			local dropbar_api = require('dropbar.api')
-			vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-			vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-			vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-		end
 	},
 }
