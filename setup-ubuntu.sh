3e13eee4da36b02e1692000668ca69b950ffcb6a
Stefan Genov <sgenov94@gmail.com>
2025-10-26 20:29:55 +0200

    feat: Neovim ubuntu installation with bare minimum is done

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# DEPENDENCIES
+
+sudo apt update
+sudo apt install -y software-properties-common ripgrep git make curl gcc
+
+# FZF
+git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME"/.fzf
+"$HOME"/.fzf/install --all
+
+# DEPENDENCIES
+
 # Neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install -y neovim
-
-# Dependencies
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-sudo apt-get install -y ripgrep
