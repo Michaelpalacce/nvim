849dbf564a02c067e8f122a3b6f8eae5f8eecd33
Stefan Genov <sgenov94@gmail.com>
2025-10-27 08:00:10 +0200

    feat: `dropbar` promoted to stable

--- lua/plugins/experimental.lua
+++ lua/plugins/experimental.lua
@@ -1,26 +1,2 @@
 return {
-	{
-		'Bekaboo/dropbar.nvim',
-		opts = {
-			bar = {
-				-- Display only the file path
-				sources = function(buf, _)
-					local sources = require('dropbar.sources')
-					if vim.bo[buf].ft == 'markdown' then
-						return {
-							sources.path,
-						}
-					end
-					if vim.bo[buf].buftype == 'terminal' then
-						return {
-							sources.terminal,
-						}
-					end
-					return {
-						sources.path,
-					}
-				end,
-			},
-		},
-	},
 }
