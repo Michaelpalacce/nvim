441d64941bea963e21aab007dd86b50b8a9e9b9c
Stefan Genov <sgenov94@gmail.com>
2025-10-28 18:08:16 +0200

    feat: Add Markview

--- lua/plugins/experimental.lua
+++ lua/plugins/experimental.lua
@@ -1,2 +1,10 @@
 return {
+	{
+		"OXY2DEV/markview.nvim",
+		lazy = false,
+		dependencies = { "saghen/blink.cmp" },
+		keys = {
+			{ "<leader>m", function() vim.cmd.Markview() end, mode = "n", desc = "Toggle [M]arkview", },
+		},
+	},
 }
