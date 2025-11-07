70057f68285ccb2d200984b9e9b841fdc558f5be
Stefan Genov <sgenov94@gmail.com>
2025-11-07 16:54:31 +0100

    feat: Session saving

--- lua/plugins/experimental.lua
+++ lua/plugins/experimental.lua
@@ -7,4 +7,22 @@ return {
 			{ "<leader>m", function() vim.cmd.Markview() end, mode = "n", desc = "Toggle [M]arkview", },
 		},
 	},
+	{
+		"folke/persistence.nvim",
+		event = "VeryLazy",
+		opts = {
+			dir = vim.fn.stdpath("state") .. "/sessions/",
+			need = 1,
+			branch = true,
+		},
+		keys = {
+			{ "<leader>qs", function() require("persistence").load() end,                mode = "n", },
+			-- select a session to load
+			{ "<leader>qS", function() require("persistence").select() end,              mode = "n" },
+			-- load the last session
+			{ "<leader>ql", function() require("persistence").load({ last = true }) end, mode = "n" },
+			-- stop Persistence => session won't be saved on exit
+			{ "<leader>qd", function() require("persistence").stop() end,                mode = "n" },
+		}
+	}
 }
