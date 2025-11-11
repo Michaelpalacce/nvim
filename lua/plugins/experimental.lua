af973227e0f52a928ac18d1830fb11513f0ef868
Stefan Genov <sgenov94@gmail.com>
2025-11-11 10:03:44 +0200

    feat(markview): Disable

--- lua/plugins/experimental.lua
+++ lua/plugins/experimental.lua
@@ -1,13 +1,5 @@
 return {
 	{
-		"OXY2DEV/markview.nvim",
-		lazy = false,
-		dependencies = { "saghen/blink.cmp" },
-		keys = {
-			{ "<leader>m", function() vim.cmd.Markview() end, mode = "n", desc = "Toggle [M]arkview", },
-		},
-	},
-	{
 		"folke/persistence.nvim",
 		event = "VeryLazy",
 		opts = {
