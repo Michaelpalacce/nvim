b5edd3a88d375ae395b119301223f8959e9b1f24
Stefan Genov <sgenov94@gmail.com>
2025-11-12 11:51:30 +0200

    feat(nvim-treesitter-context)!: Disabled

---
+++ lua/disabled/nvim-treesitter-context.lua
@@ -0,0 +1,8 @@
+return {
+
+	{
+		'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
+		config = true,
+		event = "BufRead"
+	},
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -44,9 +44,4 @@ return {
 			}
 		end
 	},
-	{
-		'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
-		config = true,
-		event = "BufRead"
-	},
 }
