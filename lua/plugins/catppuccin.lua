d36c5baf7a6fb50f4bf5b2827a3568051b6b6ea2
Stefan Genov <sgenov94@gmail.com>
2026-05-04 09:40:26 +0300

    feat: New treesitter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -63,34 +63,22 @@ return {
 				color_overrides = {},
 				custom_highlights = {},
 				default_integrations = true,
-				auto_integrations = true,
+				auto_integrations = false,
 				integrations = {
-					-- cmp = true,
-					-- gitsigns = true,
-					-- nvimtree = true,
-					-- notify = false,
-					-- -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-					--
-					-- fidget = true,
-					-- harpoon = true,
-					-- treesitter_context = true,
-					-- fzf = true,
-					-- mason = true,
-					-- neotest = true,
-					-- blink_cmp = true,
-					-- ufo = true,
-					-- telescope = {
-					-- 	enabled = true,
-					-- },
-					-- native_lsp = {
-					-- 	enabled = true,
-					-- },
-					-- treesitter = true,
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 				},
 			})
 
 			-- setup must be called before loading
-			vim.cmd.colorscheme "catppuccin"
+			vim.cmd.colorscheme "catppuccin-nvim"
 		end,
 	}, -- Theme
 }
